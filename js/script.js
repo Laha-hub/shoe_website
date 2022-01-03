ec192d58d9246f3b0a52922530b9f564c5db4c69
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2022-01-03 23:20:18 +0900

    fix products section

--- js/script.js
+++ js/script.js
@@ -19,39 +19,42 @@
         }
     };
 
-
     // product detail
-    const productPreviewContainer = document.querySelector('.products-preview-container'),
-        productPreview = document.querySelectorAll('.products-preview-container .product-preview');
+    const productPreviewContainer = document.querySelector(".products-preview-container"),
+        productPreview = document.querySelectorAll(".products-preview-container .product-preview");
 
-    document.querySelectorAll('.products .slide .btn').forEach(detailBtn => {
-        detailBtn.addEventListener('click', () => {
-            productPreviewContainer.style.display = 'block';
-            const name = detailBtn.getAttribute('data-product');
-            productPreview.forEach(preview => {
-                const target = preview.getAttribute('data-target');
-                if(name === target) {
-                    preview.style.display = 'flex';
+    document.querySelectorAll(".products .slide .btn").forEach((detailBtn) => {
+        detailBtn.addEventListener("click", () => {
+            productPreviewContainer.style.display = "block";
+            const name = detailBtn.getAttribute("data-product");
+            productPreview.forEach((preview) => {
+                const target = preview.getAttribute("data-target");
+                if (name === target) {
+                    preview.style.display = "flex";
                 }
             });
         });
     });
 
-    document.querySelectorAll('.products-preview-container .product-preview .fa-times').forEach(close => {
-        close.addEventListener('click', () => {
-            productPreviewContainer.style.display = 'none';
-            productPreview.forEach(closePreview => {
-                closePreview.style.display = 'none';
+    document.querySelectorAll(".products-preview-container .product-preview .fa-times").forEach((close) => {
+        close.addEventListener("click", () => {
+            productPreviewContainer.style.display = "none";
+            productPreview.forEach((closePreview) => {
+                closePreview.style.display = "none";
             });
         });
     });
 
     // swiper.js (products)
     var swiper = new Swiper(".products-slider", {
-        loop: true,
+        // loop: true,
         spaceBetween: 20,
         grabCursor: true,
         centeredSlides: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
         breakpoints: {
             0: {
                 slidesPerView: 1,
@@ -59,12 +62,11 @@
             768: {
                 slidesPerView: 2,
             },
-            991: {
+            1440: {
                 slidesPerView: 3,
             },
         },
     });
-
 
     // swiper.js (reviews)
     var swiper = new Swiper(".reviews-slider", {
@@ -74,13 +76,13 @@
         centeredSlides: true,
         breakpoints: {
             0: {
-                slidesPerView: 1,
+                slidesPerView: 1.6,
             },
             768: {
                 slidesPerView: 2,
             },
             991: {
-                slidesPerView: 3,
+                slidesPerView: 3.6,
             },
         },
     });
